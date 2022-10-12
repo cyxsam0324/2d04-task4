270e2207e000c817980bd2ffbf6b1b12dad80e3f
cyxsam0324 <115052298+cyxsam0324@users.noreply.github.com>
2022-10-12 09:37:51 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+let counter = 0
+input.onButtonPressed(Button.A, function () {
+    counter = 0
+    while (counter <= 4) {
+        led.plot(counter, 0)
+        basic.pause(1000)
+        basic.clearScreen()
+        counter += 1
+    }
+})
 basic.forever(function () {
 	
 })
